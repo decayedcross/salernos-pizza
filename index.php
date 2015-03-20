d38351dd7f407b1ffdfcaa5e5be18292621e6a95
decayedcross <decayedcross@gmail.com>
2015-03-20 15:22:17 -0700

    learning less/bower/jquery

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="salernos">
+		<title>Salerno's Pizza</title>
+		<link rel="stylesheet" type="text/css" href="css/load.css">
+		<script type="text/javascript" src="bower/respond/dest/respond.min.js"></script>
+		<script type="text/javascript" src="bower/sizzle/dist/sizzle.min.js"></script>
+		<script type="text/javascript" src="bower/html5shiv/dist/html5shiv.min.js"></script>		
+	</head>
+	<body>
+		<?php
+			require('views/header.php');
+			if(isset($_GET['pageName'])){
+				require('views/'.$_GET['pageName'].'.php');
+			}else{
+				require('views/home.php');
+			}
+			require('views/footer.php');
+		?>
+		<script type="text/javascript" src"bower/jquery/dist/jquery.min.js"></script>
+	</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,19 @@
+<nav class="menu-a" id="enc-navbar-open">
+			<div class="enc-navbar-header">
+				<a class="enc-navbar-brand">Brand</a>
+					<div class="enc-navbar-button">
+						<a href="#enc-navbar-open">&#9776;</a>
+						<a href="#">&#9776;</a>
+					</div>
+			</div>
+			<div class="enc-navbar-collapse">
+				<ul class="enc-nav">
+					<li><a href="index.php?pageName=home">Home</a></li>
+					<li><a href="index.php?pageName=about">About</a></li>
+					<li><a href="index.php?pageName=menu">Menu</a></li>
+					<li><a href="index.php?pageName=gallery">Gallery</a></li>
+					<li><a href="#">Reservations</a>
+					<li><a href="index.php?pageName=contact">Contact</a></li>
+				</ul>
+			</div>
+		</nav>
