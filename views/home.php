7e9f4a81b6f2f60562db2af408865c3b80b0c84b
decayedcross <decayedcross@gmail.com>
2015-03-23 14:13:32 -0700

    working on less for homepage

--- views/home.php
+++ views/home.php
@@ -1,109 +1,110 @@
-<section>
-	<img src="http://jellythemes.com/themes/elixir/03/images/slider/03.jpg">
-</section>
-<section>
-	<div class="wrap">
-		<h1>
-			<?php echo $home_section_h1; ?>
-		</h1>
-		<p>
-			<?php echo $home_section_p; ?>
-		</p>
-		<section>
-			<article>
-				<h2>
-					<?php echo $home_section_section_article_h2; ?>
-				</h2>
-				<p>
-					<?php echo $home_section_section_article_p; ?>
-				</p>
-				<p>
-					<?php echo $home_section_section_article_p_p; ?>
-				</p>
+<main id="home">
+	<section>
+		<img src="http://jellythemes.com/themes/elixir/03/images/slider/03.jpg">
+	</section>
+	<section>
+		<div>
+			<h1>
+				<?php echo $home_section_h1; ?>
+			</h1>
+			<p>
+				<?php echo $home_section_p; ?>
+			</p>
+			<section>
+				<article>
+					<h2>
+						<?php echo $home_section_section_article_h2; ?>
+					</h2>
+					<p>
+						<?php echo $home_section_section_article_p; ?>
+					</p>
+					<p>
+						<?php echo $home_section_section_article_p_p; ?>
+					</p>
+					<div>
+					</div>
+				</article>
 				<div>
 				</div>
-			</article>
-			<div>
-			</div>
-			<div>
-			</div>
-			<article>
-				<p>
-					<?php echo $home_section_section_article_article_p; ?>
-				</p>
-				<p>
-					<?php echo $home_section_section_article_article_p_p; ?>
-				</p>
-			</article>
-		</section>
-	</div>
-</section>
-<section>
-	<h1>	
-		<?php echo $home_section_section_section_h1 ?>		
-	</h1>
-	<h2>
-		<?php echo $home_section_section_section_h2 ?>				
-	</h2>
-	<article>
-		<h3>
-			<?php echo $home_section_section_section_article_h3 ?>	
-		</h3>
-		<p>
-			<?php echo $home_section_section_section_article_p ?>	
-		</p>
-		<p>
-			<?php echo $home_section_section_section_article_p_p ?>	
-		</p>
-	</article>
-	<article>
-		<h3>
-			<?php echo $home_section_section_section_article_h3 ?>	
-		</h3>
-		<p>
-			<?php echo $home_section_section_section_article_p ?>	
-		</p>
-		<p>
-			<?php echo $home_section_section_section_article_p_p ?>	
-		</p>
-	</article>
-	<p>
-		<?php echo $home_section_section_section_p ?>	
-	</p>
-</section>
-<section>
-	<div class="wrap">
-		<img src="http://jellythemes.com/themes/elixir/03/images/menu_logo.png">
-		<article>
-			<h2>
-				<?php echo $home_section_section_section_section_article_h2 ?>	
-			</h2>	
-		</article>
-			<h2>
-				<?php echo $home_section_section_section_section_article_article_h2 ?>	
-			</h2>
-		<article>
-		</article>
-		<article>
-			<h2>
-				<?php echo $home_section_section_section_section_article_article_article_h2 ?>	
-			</h2>
-		</article>
-		<a href="">
-			<?php echo $home_section_section_section_section_section_a ?>				
-		</a>
-	</div>
-</section>
-<section>
-	<div class="wrap">
-		<figure>
-			<img src="">
-		</figure>
-		<h2>
-			<?php echo $home_section_section_section_section_section_section_h2 ?>	
-		</h2>
-		<div>
-			
+				<div>
+				</div>
+				<article>
+					<p>
+						<?php echo $home_section_section_article_article_p; ?>
+					</p>
+					<p>
+						<?php echo $home_section_section_article_article_p_p; ?>
+					</p>
+				</article>
+			</section>
 		</div>
-	</div>
-</section>
+	</section>
+	<section>
+		<h1>	
+			<?php echo $home_section_section_section_h1 ?>		
+		</h1>
+		<h2>
+			<?php echo $home_section_section_section_h2 ?>				
+		</h2>
+		<article>
+			<h3>
+				<?php echo $home_section_section_section_article_h3 ?>	
+			</h3>
+			<p>
+				<?php echo $home_section_section_section_article_p ?>	
+			</p>
+			<p>
+				<?php echo $home_section_section_section_article_p_p ?>	
+			</p>
+		</article>
+		<article>
+			<h3>
+				<?php echo $home_section_section_section_article_h3 ?>	
+			</h3>
+			<p>
+				<?php echo $home_section_section_section_article_p ?>	
+			</p>
+			<p>
+				<?php echo $home_section_section_section_article_p_p ?>	
+			</p>
+		</article>
+		<p>
+			<?php echo $home_section_section_section_p ?>	
+		</p>
+	</section>
+	<section>
+		<div class="wrap">
+			<img src="http://jellythemes.com/themes/elixir/03/images/menu_logo.png">
+			<article>
+				<h2>
+					<?php echo $home_section_section_section_section_article_h2 ?>	
+				</h2>	
+			</article>
+				<h2>
+					<?php echo $home_section_section_section_section_article_article_h2 ?>	
+				</h2>
+			<article>
+			</article>
+			<article>
+				<h2>
+					<?php echo $home_section_section_section_section_article_article_article_h2 ?>	
+				</h2>
+			</article>
+			<a href="">
+				<?php echo $home_section_section_section_section_section_a ?>				
+			</a>
+		</div>
+	</section>
+	<section>
+		<div class="wrap">
+			<figure>
+				<img src="">
+			</figure>
+			<h2>
+				<?php echo $home_section_section_section_section_section_section_h2 ?>	
+			</h2>
+			<div>
+				
+			</div>
+		</div>
+	</section>
