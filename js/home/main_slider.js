03ecb0ac000ef4d405ed116721ddb3ec0cbf9207
antfuentes87 <antfuentes87@gmail.com>
2015-03-25 19:11:27 -0500

    finally got the slider working

--- js/home/main_slider.js
+++ js/home/main_slider.js
@@ -1,24 +1,62 @@
 $(document).ready(function() {
-  $("#slide-show").owlCarousel({
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
+  $('#home #slide-show').height($(window).height());
+
+  $("#home #slide-show").owlCarousel({
+      autoPlay: 3500,
+      navigation: true,
+      goToFirst: true,
+      goToFirstSpeed: 2000,
+      slideSpeed: 100,
+      pagination: true,
+      transitionStyle: "fade",
       singleItem: true,
-      pagination: false,
-      autoPlay: false,
-      navigationText : false,
-    responsive: true,
-    responsiveRefreshRate : 200,
-    responsiveBaseWidth: window,
+      navigationText: false,
+    afterInit: function() {
+                $('#home #slide-show').height($(window).height());
+                    $('#home #slide-show .item').height($(window).height());
+                    $('#home #slide-show .item img').height($(window).height());
+                
+                $('#home #slide-show .item img').width('auto');
+                
+                var left = ((($('#home #slide-show').width() - $('#home #slide-show .item img').width())/2));
+                if (left<0) {
+                    $('#home #slide-show .item img').css('top', '0px' );
+
+                    $('#home #slide-show .item img').css('left', left + 'px' );
+                } else {
+                    $('#home #slide-show .item img').css('left', '0px' );
+                    $('#home #slide-show .item img').height('auto');
+                    $('#home #slide-show .item img').width($(window).width());
+                    $('#home #slide-show .item img').css('top', ((($('#home #slide-show').height() - $('#home #slide-show .item img').height())/2)) + 'px' );
+                }
+
+            },
+            afterUpdate: function() {
+                $('#home #slide-show').height($(window).height());
+               
+                    $('#home #slide-show .item').height($(window).height());
+                    $('#home #slide-show .item img').height($(window).height());
+            
+                $('#home #slide-show .item img').width('auto');
+                var left = ((($('#home #slide-show').width() - $('#home #slide-show .item img').width())/2));
+                if (left<0) {
+                    $('#home #slide-show .item img').css('top', '0px' );
+                    $('#home #slide-show .item img').css('left', left + 'px' );
+                } else {
+                    $('#home #slide-show .item img').css('left', '0px' );
+                    $('#home #slide-show .item img').height('auto');
+                    $('#home #slide-show .item img').width($(window).width());
+                    $('#home #slide-show .item img').css('top', ((($('#home #slide-show').height() - $('#home #slide-show .item img').height())/2)) + 'px' );
+                }
+              }
   });
 });
 
-
- $(document).ready(function(){
-    $("#home #slide-show").height($(window).height());
-      $("#home #slide-show .item").height($(window).height());
+  $(document).ready(function(){
+      $("#home #slide-show").height($(window).height());
+      $("#home #slide-show .item img").height($(window).height());
           $(window).resize(function(){
-             $("#home #slide-show").height($(window).height());
-              $("#home #slide-show .item").height($(window).height());
+              $("#home #slide-show").height($(window).height());
+              $("#home #slide-show .item img").height($(window).height());
       });
   });
