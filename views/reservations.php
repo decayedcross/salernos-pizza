77dd7067e868332792baf5926335de3b8ae61d0c
Erik Balonier <erikibali@gmail.com>
2015-03-27 12:43:50 -0500

    continued reservations structure

--- views/reservations.php
+++ views/reservations.php
@@ -1,26 +1,53 @@
 <section>
-	<h1>
-		Reservations
-	</h1>
-	<article>
-
-	</article>
-	<figure>
-		<img>
-	</figure>
-	<h4></h4>
-	<h2></h2>
-	<h4></h4>
-	<article>
-	</article>
-	<article>
-	</article>
 	<div>
+		<h1>
+			Reservations
+		</h1>
 		<article>
+			<p>
+				Booking a table has never been so easy with free
+				& instant online restaurant reservations, booking now!!
+			</p>
+		</article>
+		<figure>
+			<img src="http://jellythemes.com/themes/elixir/03/images/reservations_logo.png">
+		</figure>
+		<h4>Welcome to </h4>
+		<h2>MAKE A RESERVATION</h2>
+		<h4>Open Hours</h4>
+		<article>
+			<strong>Sunday to Tuesday</strong> 09.00 - 24:00 
+			<strong>Friday and Sunday</strong> 08:00 - 03.00
 		</article>
 		<article>
+			+1-506-890-0179
 		</article>
+		<div>
+			<form>
+				<article>
+					<h6>
+						Book a table
+					</h6>
+					<input type="text" placeholder="Date">
+					<input type="text" placeholder="Time">
+					<input type="text" placeholder="Party">
+				</article>
+				<article>
+					<h6>
+						Contact Details
+					</h6>
+					<input type="text" placeholder="Name">
+					<input type="text" placeholder="Email">
+					<input type="text" placeholder="Phone">
+				</article>
+				<article>
+					<textarea>
+						
+					</textarea>
+				</article>
+			</form>
+		</div>
+		<input type="button">
+		<span> </span>
 	</div>
-	<input>
-	<span> </span>
 </section>
