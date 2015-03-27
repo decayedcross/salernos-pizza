fca859d24afdf48aa6ab3921d5088dd88ed600f7
Erik Balonier <erikibali@gmail.com>
2015-03-27 13:16:45 -0500

    copied ome.phps about us and less for about to about.php and about.less. Use the same structure and stylesheet.

--- views/about.php
+++ views/about.php
@@ -1,1 +1,44 @@
-about
+<section id="about-us">
+		<div>
+			<h1>
+				<?php echo $home_section_h1; ?>
+			</h1>
+			<p>
+				<?php echo $home_section_p; ?>
+			</p>
+		</div>
+</section>
+<section id="history">
+	<div>
+		<article>
+			<h2>
+				<?php echo $home_section_section_article_h2; ?>
+			</h2>
+			<p>
+				<?php echo $home_section_section_article_p; ?>
+			</p>
+			<p>
+				<?php echo $home_section_section_article_p_p; ?>
+			</p>
+			<span></span>
+		</article>
+		<div>
+			<img src="http://jellythemes.com/themes/elixir/03/images/about02.jpg">
+		</div>
+	</div>
+</section>
+<section id="history-two">
+	<div>
+		<div>
+			<img src="http://jellythemes.com/themes/elixir/03/images/about04.jpg">
+		</div>
+		<article>
+			<p>
+				<?php echo $home_section_section_article_article_p; ?>
+			</p>
+			<p>
+				<?php echo $home_section_section_article_article_p_p; ?>
+			</p>
+		</article>
+	</div>
+</section>
